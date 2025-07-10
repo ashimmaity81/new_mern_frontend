cc89e98b0572d6e0c046b3c3581886843822cb53
ashimmaity81 <maityashim81@gmail.com>
2025-07-10 11:55:09 +0530

    all done

---
+++ src/context/AppContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+const AppContext = createContext({
+  name: "Ashim",
+  count: 0,
+});
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,94 +1,180 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { Navbar } from "../components/Navbar";
 const ProfilePage = () => {
-    const [products,setProducts] = useState([]);
-    const getData = async () => {
-        try{
-            const resp = await fetch(`${import.meta.env.VITE_BACKEND_URL}/products`,{
-                method:"GET",
-            });
-            const result = await resp.json();
-            console.log("result -> ",result);
-            setProducts(result.data.products);
-        }
-        catch(err){
-            console.log("error while getting products -> ",err.message);
-        }
-    };
-    useEffect(()=> {
-        getData();
-    },[]);
-
-    const handleSubmit = async (e) => {
-        try{
-            // e.preventDefault();
-            const title = e.target.title.value;
-            const price = e.target.price.value;
-            const description = e.target.description.value;
-            const quantity = e.target.quantity.value;
-            const resp = await fetch(`${import.meta.env.VITE_BACKEND_URL}/products`,{
-                method:"POST",
-                body:JSON.stringify({
-                    title:title,
-                    price:price,
-                    description,
-                    quantity,
-                }),
-                headers:{
-                    "content-type":"application/json",
-                },
-            });
-            if(resp.status=='201'){
-                alert("Product added!");
-                getData();
-                console.log(resp);
-            }
-            else{
-                const result = await resp.json();
-                alert(`Invalid data: ${result.message}`);
-            }
-
-        }
-        catch(err){
-            console.log("cannot create product ->",err.message);
-            alert(`cannot create product : ${err.message}`);
-        };
+  const [products, setProducts] = useState([]);
+  const [editProductId, setEditProductId] = useState("");
+  const [updatedPrice, seUpdatedPrice] = useState(-1);
+  const getData = async () => {
+    try {
+      const resp = await fetch(`${import.meta.env.VITE_BACKEND_URL}/products`, {
+        method: "GET",
+      });
+      const result = await resp.json();
+      console.log("result -> ", result);
+      setProducts(result.data.products);
+    } catch (err) {
+      console.log("error while getting products -> ", err.message);
     }
-    return (
-        <div>
-            <Navbar/>
-            <div>
-                <form onSubmit={handleSubmit} className="mx-auto my-4 flex flex-col gap-5 p-6 bg-blue-200 max-w-150 rounded-md">
-                    <div className="flex gap-4">
-                        <label>Title :</label>
-                        <input placeholder="Write here ..." name="title" type="text" className="border-1 py-1 px-2 rounded-md"/>
-                    </div>
-                    <div className="flex gap-4">
-                        <label>Price :</label>
-                        <input placeholder="Enter here ..." name="price" type="number" className="border-1 py-1 px-2 rounded-md"/>
-                    </div>
-                    <div className="flex gap-4">
-                        <label>Description :</label>
-                        <input placeholder="Description here ..." name="description" type="text" className="border-1 py-1 px-2 rounded-md"/>
-                    </div>
-                    <div className="flex gap-4">
-                        <label>Quantity :</label>
-                        <input placeholder="Enter quantity ..." name="quantity" type="number" className="border-1 py-1 px-2 rounded-md"/>
-                    </div>
-                    <button className="border-1 py-1 px-2 rounded-md bg-blue-400">+ Add Product +</button>
-                </form>
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const title = e.target.title.value;
+      const price = e.target.price.value;
+      const description = e.target.description.value;
+      const quantity = e.target.quantity.value;
+      const resp = await fetch(`${import.meta.env.VITE_BACKEND_URL}/products`, {
+        method: "POST",
+        body: JSON.stringify({
+          title: title,
+          price: price,
+          description,
+          quantity,
+        }),
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+
+      if (resp.status == "201") {
+        alert("product added!");
+        getData();
+        console.log(resp);
+      } else {
+        const result = await resp.json();
+        alert(`Invalid data: ${result.message}`);
+      }
+    } catch (err) {
+      console.log("cannot create product ->", err.message);
+      alert(`cannot create product : ${err.message}`);
+    }
+  };
+
+  const handleEditProduct = async (productId) => {
+    try {
+      const res = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/products/${productId}`,
+        {
+          method: "PATCH",
+          body: JSON.stringify({
+            price: updatedPrice,
+          }),
+          headers: { "content-type": "application/json" },
+        }
+      );
+      if (res.status == 200) {
+        alert("Product Updated");
+        setEditProductId("");
+        getData();
+      } else {
+        const result = await res.json();
+        alert("Error while updating product:", result.message);
+      }
+    } catch (err) {
+      alert("Cannot udate product:", err.message);
+      console.log("Cannot update product:", err.message);
+    }
+  };
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <form
+          onSubmit={handleSubmit}
+          className="mx-auto my-4 flex flex-col gap-5 p-6 bg-blue-200 max-w-150 rounded-md"
+        >
+          <div className="flex gap-4">
+            <label>Title :</label>
+            <input
+              placeholder="Write here ..."
+              name="title"
+              type="text"
+              className="border-1 py-1 px-2 rounded-md"
+            />
+          </div>
+          <div className="flex gap-4">
+            <label>Price :</label>
+            <input
+              placeholder="Enter here ..."
+              name="price"
+              type="number"
+              className="border-1 py-1 px-2 rounded-md"
+            />
+          </div>
+          <div className="flex gap-4">
+            <label>Description :</label>
+            <input
+              placeholder="Description here ..."
+              name="description"
+              type="text"
+              className="border-1 py-1 px-2 rounded-md"
+            />
+          </div>
+          <div className="flex gap-4">
+            <label>Quantity :</label>
+            <input
+              placeholder="Enter quantity ..."
+              name="quantity"
+              type="number"
+              className="border-1 py-1 px-2 rounded-md"
+            />
+          </div>
+          <button className="border-1 py-1 px-2 rounded-md bg-blue-400">
+            + Add Product +
+          </button>
+        </form>
+      </div>
+      <div className="flex flex-wrap gap-6 justify-center">
+        {products.map((elem) => {
+          return (
+            <div
+              key={elem._id}
+              className="p-5 px-20 rounded-lg border-1 mt-4 min-w-70 bg-amber-100"
+            >
+              <p>{elem.title}</p>
+              {elem._id === editProductId ? (
+                <>
+                  <input
+                    value={updatedPrice}
+                    onChange={(e) => seUpdatedPrice(e.target.value)}
+                    className="py-1 px-2 border-1 rounded-md"
+                  /><br/>
+                  <button
+                    onClick={() => {
+                      setEditProductId("");
+                    }} className="py-0.5 px-2 border-1 rounded-md"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={() => {
+                      handleEditProduct(elem._id);
+                    }} className="py-0.5 px-2 border-1 rounded-md min-w-23"
+                  >
+                    Update
+                  </button>
+                </>
+              ) : (
+                <p>{elem.price}</p>
+              )}
+              <button
+                onClick={() => {
+                  setEditProductId(elem._id);
+                  seUpdatedPrice(elem.price);
+                }}
+                className="py-0.5 px-2 border-1 rounded-md"
+              >
+                Edit
+              </button>
             </div>
-            <div className="flex flex-wrap gap-6 justify-center">{
-                products.map((elem)=>{
-                    return (
-                        <div key={elem._id} className="p-5 px-20 rounded-lg border-1 mt-4 min-w-70 bg-amber-100">
-                            <p>{elem.title}</p>
-                            <p>{elem.price}</p>
-                        </div>
-                    )
-                })
-            }</div>
-        </div>
-    )
+          );
+        })}
+      </div>
+    </div>
+  );
 };
-export {ProfilePage};
+export { ProfilePage };
